949b35a9e756f06c17ce337ad06a79ab25e7d403
Eric Storey <estorey@gmail.com>
2019-07-05 23:12:25 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -65,11 +65,7 @@ function displayBranches() {
     .map(
       branch =>
         '<li><h3>' +
-        commit.commit.author.name +
-        ' (' +
-        commit.author.login +
-        ')</h3>' +
-        commit.commit.message +
+        branch.name +
         '</li>'
     )
     .join('')}</ul>`;

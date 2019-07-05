a0688e2ba540b1a059dd46a492e8eeac3b631fb8
Eric Storey <estorey@gmail.com>
2019-07-05 22:26:42 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -23,8 +23,7 @@ function showRepositories() {
 }
 
 function getCommits(el) {
-  const name = el.dataset.repo;
-  console.log(el)
+  const repository = el.dataset.repository;
   const req = new XMLHttpRequest();
   req.addEventListener('load', showCommits);
   req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');

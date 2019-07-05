cc3218ac868b11bdd375e182b809307ab54f1c71
Eric Storey <estorey@gmail.com>
2019-07-05 23:06:40 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,14 +9,17 @@ function getRepositories() {
 function displayRepositories() {
   var repos = JSON.parse(this.responseText);
   const repoList = `<ul>${repos
-    .map(
-      r =>
-        '<li>' +
-        r.name +
-        ' - <a href="#" data-repo="' +
-        r.name +
-        '" onclick="getCommits(this)">Get Commits</a></li>'
-    )
+    .map(repo => {
+        const dataUsername = 'data-username="' + repo.owner.login + '"';
+        const dataRepoName = 'data-repository="' + repo.name + '"';
+        return `
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+          </li>`;
+      })
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
